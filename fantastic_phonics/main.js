71a64a4e4fe12c917f17ccea622eda865d2f4d2b
Shaw <mrfloor@Sh-MBP13.local>
2025-03-01 07:00:53 +0900

    Random choice with exclusion, basic functionality

--- fantastic_phonics/main.js
+++ fantastic_phonics/main.js
@@ -1,13 +1,13 @@
-import { usagi_vocab as vocab } from './data/usagi_vocab.js'; // 141 items
-import { fp_adventure as adv } from './data/fp_adventure.js'; // 19 items
-import { letter_search as search } from './data/letter_search.js'; // 141 items
-import { alphabet_chant as chant } from './data/alphabet_chant.js'; // 5 items
+import { usagi_vocab } from './data/usagi_vocab.js'; // 141 items
+import { fp_adventure } from './data/fp_adventure.js'; // 19 items
+import { letter_search } from './data/letter_search.js'; // 141 items
+import { alphabet_chant } from './data/alphabet_chant.js'; // 5 items
 
 
 //// Settings
 
 const startYearMonth = { year: 2025, month: 'april' }; //// month must be full name, not number, capitalisation is ignored
-const durationMonths = 5;
+const durationMonths = 12;
 
 
 //// Internals
@@ -15,17 +15,7 @@ const durationMonths = 5;
 const log = console.log;
 
 const date = new Date(`${startYearMonth.year} ${startYearMonth.month} 1`);
-log(date);
-// log(date.getMonth(), date.getMonth() + 2);
-// const endDate = new Date(`${startYearMonth.year+1} ${startYearMonth.month} 1`);
-// const endDate = new Date(`${startYearMonth.year}-${date.getMonth() + durationMonths} 1`);
-// const endDateString = `${startYearMonth.year}-${date.getMonth() + 1 + durationMonths}-1`
-// log(endDateString);
-// const endDate = new Date(date.setMonth(date.getMonth() + durationMonths));
-const endDate = new Date(new Date(date).setMonth(date.getMonth() + durationMonths));
-// const endDate = new Date(endDateString);
-// log(endDate.getMonth());
-log(endDate);
+const endDate = new Date(new Date(date).setMonth(date.getMonth() + durationMonths)); //// Thanks https://stackoverflow.com/a/5645110/6270906 - How we long for the Temporal API... https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Temporal
 
 const schedule = [];
 
@@ -33,43 +23,44 @@ const used = {
   vocab:  [],
   chant:  [],
   search: [], //// every 2-5 days this should be an ADVENTURE!
-  adv:  [],
-  adv_days: 0
+  adventure: [],
+  adventure_days: 0
 }
 
 while (date < endDate) {
 
-  /*const vocabIndex = randomIndexExclude(vocab, used.vocab);
-  used.vocab.push(vocabIndex);*/
+  //// If all [usagi_vocab] have been used, remove the oldest [30] from used list
+  if (used.vocab.length >= usagi_vocab.length) used.vocab.splice(0, 30);
+  const vocabIndex = randomIndexExclude(usagi_vocab, used.vocab);
+  used.vocab.push(vocabIndex);
 
-  /*if (used.chant.length == chant.length) {
-    chantIndex = used.chant.shift()
-  } else {
-    chantIndex = randomIndexExclude(chant, used.chant);
-  }*/
+  //// If all [alphabet_chant] have been used, remove the oldest [3] from used list
+  if (used.chant.length >= alphabet_chant.length) used.chant.splice(0, 3);
+  const chantIndex = randomIndexExclude(alphabet_chant, used.chant);
+  used.chant.push(chantIndex);
 
-  // const chantIndex = (used.chant.length == chant.length) ? used.chant.shift() : randomIndexExclude(chant, used.chant);
-  // used.chant.push(chantIndex);
+  //// If all [letter_search] have been used, remove the oldest [30] from used list
+  if (used.search.length >= letter_search.length) used.search.splice(0, 30);
+  const searchIndex = randomIndexExclude(letter_search, used.search);
+  used.search.push(searchIndex);
 
-  // log(used.chant);
-  log(randomIndexExclude([2,3,4,5,6], [3,4]));
-
-  /*const searchIndex = randomIndexExclude(search, used.search);
-  used.search.push(searchIndex);*/
-
+  //// If all [fp_adventure] have been used, remove the oldest [7] from used list
+  if (used.adventure.length >= fp_adventure.length) used.adventure.splice(0, 7);
+  const adventureIndex = randomIndexExclude(fp_adventure, used.adventure);
+  used.adventure.push(adventureIndex);
 
   const row = {
-    date: date.getTime(),
-
-    vocab: null,
-    // chant: chant[chantIndex],
-    search: null
+    date: date.getTime(), //// Store date as Unix epoch
+    vocab: usagi_vocab[vocabIndex],
+    chant: alphabet_chant[chantIndex],
+    search: letter_search[searchIndex],
+    adventure: fp_adventure[adventureIndex]
   };
 
   schedule.push(row);
 
   used.adv_days += 1;
-  date.setDate(date.getDate() + 1); //// Oh, how I long for the Temporal API... https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Temporal
+  date.setDate(date.getDate() + 1);
 
 
 }
@@ -78,19 +69,29 @@ log(schedule);
 
 const frag = document.createDocumentFragment();
 schedule.forEach( row => {
-  const date = new Date(row.date);
+  const p = document.createElement('p');
 
+  const dateSpan = document.createElement('span');
+  const date = new Date(row.date);
   const month = date.toLocaleString('default', { month: 'short' });
   const weekday = date.toLocaleString('default', {  weekday: 'short' });
-  
-  const p = document.createElement('p');
-  p.textContent = `${date.getFullYear()} ${month} ${date.getDate()} - ${weekday}`;
+  dateSpan.textContent = `${date.getFullYear()} ${month} ${date.getDate()} - ${weekday}`;
+  p.appendChild(dateSpan);
+
+  const vocabSpan = document.createElement('span');
+  vocabSpan.textContent = `${row.vocab.object.letter} - ${row.vocab.object.word}`;
+  p.appendChild(vocabSpan);
+
+  const searchSpan = document.createElement('span');
+  searchSpan.textContent = `${row.search.word} - ${row.search.video}`;
+  p.appendChild(searchSpan);
   
   frag.appendChild(p);
 });
 
 document.body.appendChild(frag);
 
+log(used.vocab)
 
 
 

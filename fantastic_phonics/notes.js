f2af2901a536fd0ae2992740c85042532c6f7bec
nova-shaw <cunningham_shaw@nova.co.jp>
2025-02-28 18:08:19 +0900

    Initial commit

---
+++ fantastic_phonics/notes.js
@@ -0,0 +1,21 @@
+/*
+Fantastic Phonics
+
+v1 - Fantastic Phonics
+v2 - Fantastic Phonics Adventure - 2 per week
+
+Alphabet Chant
+
+Vehicles
+Actions
+Food
+Animals-1
+Animals-2
+
+Final output:
+var data = {"alphaChants": ["Vehicles", "Actions", "Animals-2", "Vehicles", "Food", "Animals", "Actions"], "letterSearches": ["anp-pan-can-cw", "TEN", "fin-fin-pin-pq", "amt-mat-map-kp", "DAD", "ags-gas-gap-np", "anv-van-pan-bp"], "usagiObjects": ["fishing-flute", "", "winter-twig", "shopping-vest", "", "traveloz-spade", "shopping-glass"]};
+
+
+*/
+
+const alphabet_chant = ['Vehicles', 'Actions', 'Food', 'Animals-1', 'Animals-2' ];
